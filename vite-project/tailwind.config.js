8f7a7409ddf72844ed5a3b39fe60c6004d01a322
obasco <oobayemi3@gmail.com>
2025-12-28 01:26:07 +0100

    hhh

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#6FA3E3",
-        dark: "#1F1F1F",
-        light: "#F2F2F2",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
